bde678c440217b383018980463b76ea55b617ec9
sriyabro <sriyabro@gmail.com>
2022-07-13 17:23:19 +0530

    keypad test changes

--- tests/keypad-4x4/keypad_test.ino
+++ tests/keypad-4x4/keypad_test.ino
@@ -2,18 +2,20 @@
 
 const byte ROWS = 4; //four rows
 const byte COLS = 4; //four columns
-//define the cymbols on the buttons of the keypads
+
+// key map
 char hexaKeys[ROWS][COLS] = {
-  {'0','1','2','3'},
-  {'4','5','6','7'},
-  {'8','9','A','B'},
-  {'C','D','E','F'}
+  {'1','2','3','A'},
+  {'4','5','6','B'},
+  {'7','8','9','C'},
+  {'*','0','#','D'}
 };
-byte rowPins[ROWS] = {3, 2, 1, 0}; //connect to the row pinouts of the keypad
-byte colPins[COLS] = {7, 6, 5, 4}; //connect to the column pinouts of the keypad
+
+byte rowPins[ROWS] = {2, 3, 4, 5}; // D2, D3, D4, D5 pins
+byte colPins[COLS] = {6, 7, 8, 9}; // D6, D7, D8, D9 pins
 
 //initialize an instance of class NewKeypad
-Keypad customKeypad = Keypad( makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS); 
+Keypad customKeypad = Keypad(makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS); 
 
 void setup(){
   Serial.begin(9600);
