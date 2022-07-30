3b2b6551037eac353333aee60c497016fb43c335
sriyabro <sriyabro@gmail.com>
2022-07-30 07:42:58 +0530

    lcd menu added, keypad input &  new vol chnages

--- liquid-filler/liquid-filler.ino
+++ liquid-filler/liquid-filler.ino
@@ -2,8 +2,8 @@
  * @file liquid-filler.ino
  * @author Sriyanjith Herath (sriyabro@gmail.com)
  * @brief Automated liquid filler machine firmware for the Arduino Mega 2560 R3 .
- * @version 4.0
- * @date 2022-07-13
+ * @version 4.1
+ * @date 2022-07-30
  *
  * @copyright  Copyright 2021 Sriyanjith Herath. All rights reserved. Unauthorized access, copying, publishing, sharing, reuse of algorithms, concepts, design patterns and code level demonstrations are strictly prohibited without any written approval of the author.
  */
@@ -22,13 +22,13 @@
 
   -- FILLING VOLUME SELECTOR BUTTONS
   250 ml - D4 - PG5
-  500 ml - D5 - PE3
-  750 ml - D6 - PH3
-  1000 ml - D7 - PH4
-  2000 ml - D8 - PH5
-  2500 ml - D9 - PH6
-  5000 ml - D10 - PB4
-  5500ml - D24 - PA2
+  330 ml - D5 - PE3
+  500 ml - D6 - PH3
+  675 ml - D7 - PH4
+  750 ml - D8 - PH5
+  1000 ml - D9 - PH6
+  2000 ml - D10 - PB4
+  5000 ml - D24 - PA2
 
   -- LIMIT SWITCHES
   nozzelLimit - D3 - PE5                 // Limit switch at nozzel
@@ -49,6 +49,26 @@
   ROWS - D26, D27, D28, D29
   Cols - D30, D31, D32, D33
 
+-- MENU INVOKE BUTTON
+  menuBtn - D19 - PD2
+
+-- KEYPAD BUTTON MAP
+  1 - '1'
+  2 - '2'
+  3 - '3'
+  4 - '4'
+  5 - '5'
+  6 - '6'
+  7 - '7'
+  8 - '8'
+  9 - '9'
+  0 - '0'
+  # - '#'
+  BACK - '*'
+  UP - 'A'
+  DOWN - 'B'
+  CLEAR - 'C'
+  OK - 'D'
 
 **/
 
@@ -77,24 +97,27 @@ const int downButton = A3;
 
 // VOLUME SELECTOR BUTTONS PINS
 const int _250 = 4;
-const int _500 = 5;
-const int _750 = 6;
-const int _1000 = 7;
-const int _2000 = 8;
-const int _2500 = 9;
-const int _5000 = 10;
-const int _5500 = 24;
+const int _330 = 5;
+const int _500 = 6;
+const int _675 = 7;
+const int _750 = 8;
+const int _1000 = 9;
+const int _2000 = 10;
+const int _5000 = 24;
 
-// MANUAL FILL BTN
+// MANUAL FILL BTN PIN
 const int manualFillBtn = 25; // manual fill button - press hold to fill
 
-// LIMIT SWITCHES
+// LIMIT SWITCHES PINS
 const int nozzelLimit = 3;     // Limit switch at nozzel
 const int topBottomLimit = 13; // Limit switches at top & bottom
 
 // KEYPAD PINS
 byte rowPins[4] = {26, 27, 28, 29};
 byte colPins[4] = {30, 31, 32, 33};
+
+// MENU BUTTON PIN
+const int menuBtn = 19; // menu button
 
 /*--------------------------------------------------------------------------
  * INITAILIZE VARIABLES
@@ -128,15 +151,60 @@ bool nozzelLimitReached = false;
 bool limitReached = false;
 
 // key map
-char keyMap[ROWS][COLS] = {
-  {'1','2','3','A'},
-  {'4','5','6','B'},
-  {'7','8','9','C'},
-  {'*','0','#','D'}
+char keyMap[4][4] = {
+    {'1', '2', '3', 'A'},
+    {'4', '5', '6', 'B'},
+    {'7', '8', '9', 'C'},
+    {'*', '0', '#', 'D'}};
+Keypad keypad = Keypad(makeKeymap(keyMap), rowPins, colPins, 4, 4);
+
+String menuItems[] = {"250 ml", "330 ml", "500 ml", "675 ml", "750 ml", "1000 ml", "2000 ml", "5000ml"};
+
+bool menuInvoked = false;
+// Navigation button variables
+int readKey;
+int key;
+// Menu control variables
+int menuPage = 0;
+int maxMenuPages = round(((sizeof(menuItems) / sizeof(String)) / 2) + .5);
+int cursorPosition = 0;
+int result = 0;
+
+String inputString = ""; // number input string
+
+// Custom characters for the menu display
+byte downArrow[8] = {
+    0b00100, //   *
+    0b00100, //   *
+    0b00100, //   *
+    0b00100, //   *
+    0b00100, //   *
+    0b10101, // * * *
+    0b01110, //  ***
+    0b00100  //   *
 };
-Keypad keypad = Keypad(makeKeymap(keyMap), rowPins, colPins, 4, 4); 
 
+byte upArrow[8] = {
+    0b00100, //   *
+    0b01110, //  ***
+    0b10101, // * * *
+    0b00100, //   *
+    0b00100, //   *
+    0b00100, //   *
+    0b00100, //   *
+    0b00100  //   *
+};
 
+byte menuCursor[8] = {
+    B01000, //  *
+    B00100, //   *
+    B00010, //    *
+    B00001, //     *
+    B00010, //    *
+    B00100, //   *
+    B01000, //  *
+    B00000  //
+};
 /*--------------------------------------------------------------------------
  * SETUP
  */
@@ -145,13 +213,13 @@ void setup()
   pinMode(solenoidValveRelay, OUTPUT);
 
   pinMode(_250, INPUT_PULLUP);
+  pinMode(_330, INPUT_PULLUP);
   pinMode(_500, INPUT_PULLUP);
+  pinMode(_675, INPUT_PULLUP);
   pinMode(_750, INPUT_PULLUP);
   pinMode(_1000, INPUT_PULLUP);
   pinMode(_2000, INPUT_PULLUP);
-  pinMode(_2500, INPUT_PULLUP);
   pinMode(_5000, INPUT_PULLUP);
-  pinMode(_5500, INPUT_PULLUP);
 
   pinMode(stepperPulse, OUTPUT);
   pinMode(stepperDir, OUTPUT);
@@ -169,13 +237,21 @@ void setup()
 
   // initailize pins
   digitalWrite(solenoidValveRelay, HIGH); // Turn OFF pump
-  digitalWrite(pumpDir, LOW);            // Set pump direction - Change HIGH to LOW to move the pump in the opposite direction
+  digitalWrite(pumpDir, LOW);             // Set pump direction - Change HIGH to LOW to move the pump in the opposite direction
 
-  lcd.begin();
+  lcd.begin(20,4);
   lcd.backlight();
   startUpMesaage();
 
   Serial.begin(9600);
+
+  keypad.addEventListener(keypadEvent); // Add an event listener for this keypad
+  lcd.createChar(0, menuCursor);
+  lcd.createChar(1, upArrow);
+  lcd.createChar(2, downArrow);
+
+  pinMode(menuBtn, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(menuBtn), setMenuInvoke, LOW);
 }
 
 /*--------------------------------------------------------------------------
@@ -208,6 +284,10 @@ void loop()
         runManualFill();
       }
     }
+  }
+  if (menuInvoked)
+  {
+    menu();
   }
 }
 
@@ -262,45 +342,45 @@ void getVolumeToFill()
     volPerSec = 29.7;
     pumpSteps = 1000;
   }
+  else if (digitalRead(_330) == LOW)
+  {
+    volumeToFill = 330;
+    volPerSec = 29.5;
+    pumpSteps = 1000;
+  }
   else if (digitalRead(_500) == LOW)
   {
     volumeToFill = 500;
-    volPerSec = 29.5;
+    volPerSec = 29.4;
+    pumpSteps = 1000;
+  }
+  else if (digitalRead(_675) == LOW)
+  {
+    volumeToFill = 675;
+    volPerSec = 30.5;
     pumpSteps = 1000;
   }
   else if (digitalRead(_750) == LOW)
   {
     volumeToFill = 750;
-    volPerSec = 29.4;
+    volPerSec = 32.8;
     pumpSteps = 1000;
   }
   else if (digitalRead(_1000) == LOW)
   {
     volumeToFill = 1000;
-    volPerSec = 30.5;
+    volPerSec = 36.4;
     pumpSteps = 1000;
   }
   else if (digitalRead(_2000) == LOW)
   {
     volumeToFill = 2000;
-    volPerSec = 32.8;
-    pumpSteps = 1000;
-  }
-  else if (digitalRead(_2500) == LOW)
-  {
-    volumeToFill = 2500;
     volPerSec = 36.4;
     pumpSteps = 1000;
   }
   else if (digitalRead(_5000) == LOW)
   {
     volumeToFill = 5000;
-    volPerSec = 36.4;
-    pumpSteps = 1000;
-  }
-  else if (digitalRead(_5500) == LOW)
-  {
-    volumeToFill = 5500;
     volPerSec = 36.4;
     pumpSteps = 1000;
   }
@@ -394,8 +474,281 @@ void safeReturn()
   delay(1000);
 }
 
-// LCD -----------------------------------------------
+void setMenuInvoke()
+{
+  menuInvoked = true;
+}
 
+void menu()
+{
+  mainMenuDraw();
+  drawCursor();
+  operateMainMenu();
+}
+
+void keypadEvent(KeypadEvent key)
+{
+  switch (keypad.getState())
+  {
+    int readKey;
+  case PRESSED:
+    break;
+  }
+}
+
+void mainMenuDraw()
+{
+  lcd.clear();
+  lcd.setCursor(1, 0);
+  lcd.print(menuItems[menuPage]);
+  lcd.setCursor(1, 1);
+  lcd.print(menuItems[menuPage + 1]);
+  lcd.setCursor(1, 2);
+  lcd.print(menuItems[menuPage + 2]);
+  lcd.setCursor(1, 3);
+  lcd.print(menuItems[menuPage + 3]);
+  if (menuPage == 0)
+  {
+    lcd.setCursor(15, 3);
+    lcd.write(byte(2));
+  }
+  else if (menuPage > 0 and menuPage < maxMenuPages)
+  {
+    lcd.setCursor(15, 3);
+    lcd.write(byte(2));
+    lcd.setCursor(15, 0);
+    lcd.write(byte(1));
+  }
+  else if (menuPage == maxMenuPages)
+  {
+    lcd.setCursor(15, 0);
+    lcd.write(byte(1));
+  }
+}
+
+void drawCursor()
+{
+  for (int x = 0; x < 4; x++)
+  { // Erases current cursor
+    lcd.setCursor(0, x);
+    lcd.print(" ");
+  }
+
+  if (menuPage % 2 == 0)
+  {
+    if (cursorPosition % 2 == 0)
+    { // If the menu page is even and the cursor position is even that means the cursor should be on line 1
+      lcd.setCursor(0, 0);
+      lcd.write(byte(0));
+    }
+    if (cursorPosition % 2 != 0)
+    { // If the menu page is even and the cursor position is odd that means the cursor should be on line 2
+      lcd.setCursor(0, 1);
+      lcd.write(byte(0));
+    }
+  }
+  if (menuPage % 2 != 0)
+  {
+    if (cursorPosition % 2 == 0)
+    { // If the menu page is odd and the cursor position is even that means the cursor should be on line 2
+      lcd.setCursor(0, 1);
+      lcd.write(byte(0));
+    }
+    if (cursorPosition % 2 != 0)
+    { // If the menu page is odd and the cursor position is odd that means the cursor should be on line 1
+      lcd.setCursor(0, 0);
+      lcd.write(byte(0));
+    }
+  }
+}
+
+void operateMainMenu()
+{
+  int activeButton = 0;
+  while (activeButton == 0)
+  {
+    int button;
+    delay(500); // [DEBUG]
+    button = evaluateButton(readKey);
+    Serial.println(button); // [DEBUG]
+    if (button == 42)
+    {
+      menuInvoked = false;
+      break;
+    }
+
+    switch (button)
+    {
+    case 0: // When button returns as 0 there is no action taken
+      break;
+    case 66: // down
+      button = 0;
+      if (menuPage % 2 == 0 and cursorPosition % 2 != 0)
+      {
+        menuPage = menuPage + 1;
+        menuPage = constrain(menuPage, 0, maxMenuPages);
+      }
+
+      if (menuPage % 2 != 0 and cursorPosition % 2 == 0)
+      {
+        menuPage = menuPage + 1;
+        menuPage = constrain(menuPage, 0, maxMenuPages);
+      }
+
+      cursorPosition = cursorPosition + 1;
+      cursorPosition = constrain(cursorPosition, 0, ((sizeof(menuItems) / sizeof(String)) - 1));
+
+      Serial.print("menuPage: ");       // [DEBUG]
+      Serial.println(menuPage);         // [DEBUG]
+      Serial.print("cursorPosition: "); // [DEBUG]
+      Serial.println(cursorPosition);   // [DEBUG]
+
+      mainMenuDraw();
+      drawCursor();
+      activeButton = 1;
+      break;
+    case 65: // up
+      button = 0;
+      if (menuPage == 0)
+      {
+        cursorPosition = cursorPosition - 1;
+        cursorPosition = constrain(cursorPosition, 0, ((sizeof(menuItems) / sizeof(String)) - 1));
+      }
+      if (menuPage % 2 == 0 and cursorPosition % 2 == 0)
+      {
+        menuPage = menuPage - 1;
+        menuPage = constrain(menuPage, 0, maxMenuPages);
+      }
+
+      if (menuPage % 2 != 0 and cursorPosition % 2 != 0)
+      {
+        menuPage = menuPage - 1;
+        menuPage = constrain(menuPage, 0, maxMenuPages);
+      }
+
+      cursorPosition = cursorPosition - 1;
+      cursorPosition = constrain(cursorPosition, 0, ((sizeof(menuItems) / sizeof(String)) - 1));
+
+      Serial.print("menuPage: ");       // [DEBUG]
+      Serial.println(menuPage);         // [DEBUG]
+      Serial.print("cursorPosition: "); // [DEBUG]
+      Serial.println(cursorPosition);   // [DEBUG]
+
+      mainMenuDraw();
+      drawCursor();
+      activeButton = 1;
+      break;
+    case 68: // This case will execute if the "OK" button is pressed
+      button = 0;
+      activeButton = 1;
+
+      Serial.print("menuPage: ");       // [DEBUG]
+      Serial.println(menuPage);         // [DEBUG]
+      Serial.print("cursorPosition: "); // [DEBUG]
+      Serial.println(cursorPosition);   // [DEBUG]
+
+      menuItem(cursorPosition);
+      mainMenuDraw();
+      drawCursor();
+      break;
+    default:
+      break;
+    }
+  }
+}
+
+void menuItem(int itemId) // itemId - cursorPointer
+{                         // Function executes when you select a item from main menu
+  int activeButton = 0;
+
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print(menuItems[itemId]);
+
+  while (activeButton == 0)
+  {
+    char key = keypad.getKey();
+    if (key)
+    {
+      Serial.println(key); // [DEBUG]
+
+      if (key == 'D')
+      {
+        if (inputString.length() > 0)
+        {
+          int inputInt = inputString.toInt();
+          saveInput(itemId, inputInt);
+          inputString = ""; // clear input
+        }
+      }
+      else if (key == 'C')
+      {
+        inputString = ""; // clear input
+      }
+      else if (key == '*')
+      {
+        inputString = ""; // clear input
+        activeButton = 1;
+      }
+      if (key >= '0' && key <= '9')
+      {                      // only act on numeric keys
+        Serial.println(key); // [DEBUG]
+        inputString += key;
+        Serial.print("inputString: "); // [DEBUG]
+        Serial.println(inputString);   // [DEBUG]
+      }
+      lcd.setCursor(0, 1);
+      lcd.print(inputString);
+    }
+  }
+}
+
+void saveInput(int volIndex, int inputInt)
+{
+  Serial.print("Saving input - "); // [DEBUG]
+  Serial.print(menuItems[volIndex]); // [DEBUG]
+  Serial.println(" : "); // [DEBUG]
+  Serial.println(inputInt); // [DEBUG]
+  /// SAVING here
+};
+
+int evaluateButton(int x)
+{
+  int result = 0;
+  int key = keypad.getKey();
+  x = key;
+  return x;
+}
+
+// LCD -----------------------------------------------
+// print String to LCD
+void printStrToLCD(String text, int lineNo)
+{
+  lcd.setCursor(0, lineNo - 1);
+  lcd.print("                "); // Clear line before print
+  lcd.setCursor(0, lineNo - 1);
+  lcd.print(text);
+}
+
+// print variable to LCD
+void printVarToLCD(int text, int lineNo)
+{
+  lcd.setCursor(0, lineNo - 1);
+  lcd.print("                "); // clear line before print
+  lcd.setCursor(0, lineNo - 1);
+  lcd.print(text);
+}
+
+// create and print custom characters to LCD
+void printCustomChar(uint8_t location, uint8_t charmap[], int row, int col)
+{
+  lcd.createChar(location, charmap);
+  lcd.setCursor(col - 1, row - 1);
+  lcd.write(location);
+}
+
+// STARTUP MESSAGE ---------------------------------------------------
+// logo characters
 byte q11[] = {
     B01111,
     B11111,
@@ -565,32 +918,6 @@ byte c211[] = {
     B00000,
     B11111,
     B11111};
-
-// print String to LCD
-void printStrToLCD(String text, int lineNo)
-{
-  lcd.setCursor(0, lineNo - 1);
-  lcd.print("                "); // Clear line before print
-  lcd.setCursor(0, lineNo - 1);
-  lcd.print(text);
-}
-
-// print variable to LCD
-void printVarToLCD(int text, int lineNo)
-{
-  lcd.setCursor(0, lineNo - 1);
-  lcd.print("                "); // clear line before print
-  lcd.setCursor(0, lineNo - 1);
-  lcd.print(text);
-}
-
-// create and print custom characters to LCD
-void printCustomChar(uint8_t location, uint8_t charmap[], int row, int col)
-{
-  lcd.createChar(location, charmap);
-  lcd.setCursor(col - 1, row - 1);
-  lcd.write(location);
-}
 
 // Power ON startup message
 void startUpMesaage()
--- tests/keypad-4x4/keypad_test.ino
+++ tests/keypad-4x4/keypad_test.ino
@@ -1,50 +1,53 @@
 #include <Keypad.h>
 
-const byte ROWS = 4; //four rows
-const byte COLS = 4; //four columns
+const byte ROWS = 4; // four rows
+const byte COLS = 4; // four columns
 
 // key map
 char hexaKeys[ROWS][COLS] = {
-  {'1','2','3','A'},
-  {'4','5','6','B'},
-  {'7','8','9','C'},
-  {'*','0','#','D'}
-};
+    {'1', '2', '3', 'A'},
+    {'4', '5', '6', 'B'},
+    {'7', '8', '9', 'C'},
+    {'*', '0', '#', 'D'}};
 
 byte rowPins[ROWS] = {26, 27, 28, 29};
 byte colPins[COLS] = {30, 31, 32, 33};
 
-//initialize an instance of class NewKeypad
-Keypad customKeypad = Keypad(makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS); 
+// initialize an instance of class NewKeypad
+Keypad customKeypad = Keypad(makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS);
 
-void setup(){
+void setup()
+{
   Serial.begin(9600);
 }
-  
-void loop(){
+
+void loop()
+{
   char customKey = customKeypad.getKey();
-  
-  if (customKey){
+
+  if (customKey)
+  {
     Serial.println(customKey);
   }
 }
 
-// 0 - 48
-// 1 - 49
-// 2 - 50
-// 3 - 51
-// 4 - 52
-// 5 - 53
-// 6 - 54
-// 7 - 55
-// 8 - 56
-// 9 - 57
-// * - 42 - back
-// # - 35 - 
-// A - 65 - up
-// B - 66 - down
-// C - 67 - clear
-// D - 68 - OK
+/** KEY MAP
+ 
+    1 - '1'
+    2 - '2'
+    3 - '3'
+    4 - '4'
+    5 - '5'
+    6 - '6'
+    7 - '7'
+    8 - '8'
+    9 - '9'
+    0 - '0'
+    # - '#'
+    BACK - '*'
+    UP - 'A'
+    DOWN - 'B'
+    CLEAR - 'C'
+    OK - 'D'
 
-
-// 250, 330, 500, 675, 750, 1000, 2000 and 5000
+ */
